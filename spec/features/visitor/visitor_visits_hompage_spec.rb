b7d34393f71527969cdb26b456626a06caa5ba81
João Gustavo de Paula <dev.joaogustavo@gmail.com>
2016-01-25 11:47:55 -0200

    Visitor visits hompage and see jobs informations

--- spec/features/visitor/visitor_visits_hompage_spec.rb
+++ spec/features/visitor/visitor_visits_hompage_spec.rb
@@ -7,4 +7,14 @@ feature 'Visitor visits hompage' do
     expect(page).to have_content "Emprego Já"
   end
 
+  scenario 'and sees all jobs' do
+    job = Job.create(title: "Developer Ruby Junior",
+                     location: "San Francisco",
+                     company: "Xerox")
+    visit root_path
+    expect(page).to have_content job.title
+    expect(page).to have_content job.location
+    expect(page).to have_content job.company
+  end
+
 end
